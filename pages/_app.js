cb5d19e9a5df0889ef2f77c4867a0da10b97791a
rameshpunyamanthula <punyamanthularamesh53@gmail.com>
2026-01-29 23:08:45 +0530

    feat: integrate Redux provider, toast support, and expose verification helpers

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import "@/styles/globals.css";
+import { Provider } from "react-redux";
+import { store } from "../store";
+import { useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    // ✅ ALWAYS expose for GPP verification (Docker = production)
+    window.getCartState = () => store.getState().cart;
+    window.getWishlistState = () => store.getState().wishlist;
+    window.getLastToastMessage = () => window.__LAST_TOAST__ || null;
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <ToastContainer />
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
